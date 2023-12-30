81a9d5c4781f836bafd410884dfc5dd8699349e7
Jonathan Brito Costa <devjonathanbrito@gmail.com>
2023-12-30 17:16:11 -0300

    Criando o projeto e separando os modulos e criando a classe category e catgorytest

---
+++ application/src/main/java/com/brito/admin/catalogo/application/UseCase.java
@@ -0,0 +1,9 @@
+package com.brito.admin.catalogo.application;
+
+import com.brito.admin.catalogo.domain.category.Category;
+
+public class UseCase {
+    public Category execute(){
+        return new Category();
+  }
+}
---
+++ infrastructure/src/main/java/com/brito/admin/catalogo/infrastructure/Main.java
@@ -0,0 +1,11 @@
+package com.brito.admin.catalogo.infrastructure;
+
+import com.brito.admin.catalogo.application.UseCase;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println(new UseCase().execute());
+    }
+}
