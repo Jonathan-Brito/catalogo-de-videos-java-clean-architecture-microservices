f7c9a236b66b344d5071aac7c15398395097d406
Jonathan Brito Costa <devjonathanbrito@gmail.com>
2024-01-05 15:56:02 -0300

    Criando o jpa repository da categoria

--- infrastructure/src/main/java/com/brito/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/brito/admin/catalogo/infrastructure/Main.java
@@ -1,9 +1,16 @@
 package com.brito.admin.catalogo.infrastructure;
 
+import com.brito.admin.catalogo.domain.category.Category;
+import com.brito.admin.catalogo.infrastructure.category.persistence.CategoryJpaEntity;
+import com.brito.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
 import com.brito.admin.catalogo.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.AbstractEnvironment;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Main {
@@ -13,5 +20,21 @@ public class Main {
         System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
 
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner(CategoryRepository categoryRepository){
+        return args -> {
+            List<CategoryJpaEntity> all = categoryRepository.findAll();
+
+            // Criando uma nova categoria
+            Category filmes = Category.newCategory("Filme", null, true);
+
+            // Persistir
+            categoryRepository.saveAndFlush(CategoryJpaEntity.from(filmes));
+
+            // Depois ir até o banco e apagar tudo
+            categoryRepository.deleteAll();
+        };
     }
 }
--- infrastructure/src/main/java/com/brito/admin/catalogo/infrastructure/category/persistence/CategoryJpaEntity.java
+++ infrastructure/src/main/java/com/brito/admin/catalogo/infrastructure/category/persistence/CategoryJpaEntity.java
@@ -7,7 +7,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-import java.io.Serializable;
 import java.time.Instant;
 
 @Entity
