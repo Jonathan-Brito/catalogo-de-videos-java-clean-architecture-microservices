418f93e8965bbbed3f5481091cddb76ee3a949a4
Jonathan Brito Costa <devjonathanbrito@gmail.com>
2024-01-06 15:39:32 -0300

    Criando a estrategia para testes integrados de banco de dados

---
+++ infrastructure/src/test/java/com/brito/admin/catalogo/infrastructure/category/CategoryMySQLGatewayTest.java
@@ -0,0 +1,28 @@
+package com.brito.admin.catalogo.infrastructure.category;
+
+import com.brito.admin.catalogo.infrastructure.MySQLGatewayTest;
+import com.brito.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@MySQLGatewayTest
+public class CategoryMySQLGatewayTest {
+
+    @Autowired
+    private CategoryMySQLGateway categoryGateway;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    public void testInjectedDependencies(){
+        Assertions.assertNotNull(categoryGateway);
+        Assertions.assertNotNull(categoryRepository);
+    }
+
+
+
+
+
+}
